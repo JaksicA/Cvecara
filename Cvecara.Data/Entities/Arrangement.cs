6592be660e11be41d669c00edcbf24308e28c528
Aleksa <jaksicaleksa77@gmail.com>
2022-09-20 10:29:48 +0200

    Translate Arrangement views

--- Cvecara.Data/Entities/Arrangement.cs
+++ Cvecara.Data/Entities/Arrangement.cs
@@ -1,11 +1,17 @@
-﻿namespace Cvecara.Data.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Cvecara.Data.Entities
 {
     public class Arrangement : IEntity
     {
         public int Id { get; set; }
+        [Display(Name = "Ime")]
         public string Name { get; set; }
+        [Display(Name = "Opis")]
         public string Description { get; set; }
+        [Display(Name = "Putanja do slike")]
         public string ImagePath { get; set; }
+        [Display(Name = "Cena")]
         public int Price { get; set; }
     }
 }
